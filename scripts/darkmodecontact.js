569df589faa02516137169330fd76a11604fc7db
Raulinhotic <schluet0r4life@gmail,com>
2021-05-29 16:52:09 +0200

    added dark mode persistence to contact page

--- scripts/darkmodecontact.js
+++ scripts/darkmodecontact.js
@@ -9,6 +9,7 @@ element.onclick = function () {
     document.querySelector('footer').style.color = "white";
     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1604147495798-57beb5d6af73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
     document.querySelector('.banner').style.backgroundImage = "url('https://images.unsplash.com/photo-1529753253655-470be9a42781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')";
+    localStorage.setItem("darkmode", "yes");
      };
 
 lightmode.onclick = function () { 
@@ -20,4 +21,21 @@ lightmode.onclick = function () {
     document.querySelector('footer').style.color = "gray";
     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')";
     document.querySelector('.banner').style.backgroundImage = "url('https://images.unsplash.com/photo-1487088678257-3a541e6e3922?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')";
+    localStorage.clear()
     };
+
+    function getLocalStorage() {
+        if (localStorage.getItem("darkmode")) {
+            element.style.display = "none";
+            lightmode.style.display = "initial";
+            document.querySelector('#contact').style.backgroundColor = "black";
+            document.querySelector('#contact').style.border = "1px solid black";
+            document.querySelector('nav').style.backgroundColor = "black";
+            document.querySelector('footer').style.color = "white";
+            document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1604147495798-57beb5d6af73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')";
+            document.querySelector('.banner').style.backgroundImage = "url('https://images.unsplash.com/photo-1529753253655-470be9a42781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')";
+            localStorage.setItem("darkmode", "yes");
+        }
+      }
+      
+      document.addEventListener("DOMContentLoaded", getLocalStorage);
